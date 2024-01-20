026c4cea708015020e60802ca421f6ac494d2cc8
Jingyu Oh <ojg1993@gmail.com>
2024-01-20 18:20:33 +0900

    style: fix lint

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,6 +18,6 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database is not available, wating 1 second...')
+                self.stdout.write('Database not available, wating 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
