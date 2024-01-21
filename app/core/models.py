16f78171fdf8d17190a40995234f54ead6c12db8
Jingyu Oh <ojg1993@gmail.com>
2024-01-21 12:57:54 +0900

    feat: customise and implement user functionality

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,40 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    def create_user(self, email, password=None, **extra_fields):
+        # Create, save and return a user with the given email & password
+        if not email:
+            raise ValueError("User must have an email address.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        # Create and save a SuperUser with the given email and password
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
