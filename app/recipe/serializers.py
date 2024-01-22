f278275d69fb9ae312a9f73a78aa9de847b6967a
Jingyu <ojg1993@gmail.com>
2024-01-23 01:19:08 +0900

    feat: implement ingredient api functionality
    - Uncommented test_commands.py for Docker based env

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from core import models
 
 
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
@@ -11,14 +18,17 @@ class TagSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = models.Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link',
+                  'tags', 'ingredients'
+                  ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
-        # Handle getting or creating tags as needed
+        # Handle getting or creating tags as requested
         auth_user = self.context['request'].user
         for tag in tags:
             # Get if already exists, else create -> get_or_create
@@ -28,20 +38,38 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        # Handle getting or creating ingredients as requested
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            # Get if already exists, else create -> get_or_create
+            ingredient_obj, created = models.Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient  # {"name": "Ingredient Name"}
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         # Create a recipe
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = models.Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
     def update(self, instance, validated_data):
         # Update recipe
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -28,15 +28,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 viewsets.GenericViewSet):
-    # Manage tags
-    serializer_class = serializers.TagSerializer
-    queryset = Tag.objects.all()
+class BaseRecipeAttrViewSet(mixins.ListModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.DestroyModelMixin,
+                            viewsets.GenericViewSet):
+    # Base viewset for recipe attributes
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    # Manage tags
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    # Manage ingredients
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
